c0940516ea4008f7f5e9360f8d5f86b42bdb3438
Sergei Pastushenko <sergei6463597314@gmail.com>
2025-01-28 19:51:22 -0500

    gitusersearch test

--- js/gituser.js
+++ js/gituser.js
@@ -14,21 +14,48 @@ form.addEventListener("submit", async (e) => {
         return;
     }
 
+    userInfoContainer.innerHTML = `<p>Loading...</p>`;
+    // reposContainer.innerHTML = "";
+
     try {
         const userResponse = await fetch(`${API_GITHUB}/${username}`);
         if (!userResponse.ok) throw new Error("User not found");
+
         const userData = await userResponse.json();
 
         userInfoContainer.innerHTML = `
             <div>
-                <img src="${userData.avatar_url}" alt="${userData.login}'s avatar" />
+                <img src="${userData.avatar_url}" alt="${userData.login}"/>
                 <h3>${userData.name || userData.login}</h3>
                 <p>${userData.bio || "No Bio Available"}</p>
             </div>
         `;
 
+        const reposResponse = await fetch(userData.repos_url);
+        if (!reposResponse.ok) throw new Error("Repos not found");
+
+        const repos = await reposResponse.json();
+
+        console.log(userInfoContainer)
+
+        if (repos.length) {
+            reposContainer.innerHTML = `<h3>Repositories:</h3>`;
+
+            repos.forEach(repo => {
+                reposContainer.innerHTML += `
+                <div class="repos-container">
+                <a href="${repo.html_url}" target=_blank>${repo.name}</a>
+                </div>
+                ` 
+            })
+
+        } else {
+            reposContainer.innerHTML = `<p>No repositories found</p>`;
+        }
        
     } catch (error) {
         userInfoContainer.innerHTML = `<p>${error.message}</p>`;
     }
 });
+
+
